d5a0c91962dc5a0b0daefa55f2d8f2076fded083
SzalDom <makos.men54@gmail.com>
2025-05-28 14:58:38 +0200

    Create JSONReadSIYKJ5.java

---
+++ SIYKJ5_05.07./src/SIYKJ5/JSONReadSIYKJ5.java
@@ -0,0 +1,50 @@
+package SIYKJ5;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+public class JSONReadSIYKJ5 {
+    public static void main(String[] args) {
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject root = (JSONObject) parser.parse(new FileReader("orarendSIYKJ5.json"));
+
+            parseObject(root, "");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void parseObject(JSONObject jsonObject, String indent) {
+        for (Object key : jsonObject.keySet()) {
+            Object value = jsonObject.get(key);
+
+            if (value instanceof JSONObject) {
+                System.out.println(indent + key + ":");
+                parseObject((JSONObject) value, indent + "  ");
+            } else if (value instanceof JSONArray) {
+                System.out.println(indent + key + ":");
+                parseArray((JSONArray) value, indent + "  ");
+            } else {
+                System.out.println(indent + key + ": " + value);
+            }
+        }
+    }
+
+    public static void parseArray(JSONArray array, String indent) {
+        for (Object element : array) {
+            if (element instanceof JSONObject) {
+                parseObject((JSONObject) element, indent);
+            } else if (element instanceof JSONArray) {
+                parseArray((JSONArray) element, indent + "  ");
+            } else {
+                System.out.println(indent + "Elem: " + element);
+            }
+        }
+    }
+}
